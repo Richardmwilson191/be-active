555c6f19538d189fb3450f161eb5f62d6eafc0dc
Richard Wilson <richardmwilson191@gmail.com>
2020-06-02 22:34:42 -0500

    Changed connection to pool and some other minor changes

--- app.js
+++ app.js
@@ -11,46 +11,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-
-  console.log('connected as id ' + connection.threadId);
-});
-
 app.get('/', function (req, res) {
   let app_data = [];
   let get_interests_query = 'select * from interests';
 
-  connection.query(get_interests_query, function (error, results, fields) {
-    if (error) throw error;
-    app_data = results;
-  });
+  pool.getConnection(function (err, connection) {
+    connection.query(get_interests_query, function (error, results, fields) {
+      if (error) throw error;
+      app_data = results;
+    });
 
-  let number_of_users_query = 'select count(*) as total from users';
+    let number_of_users_query = 'select count(*) as total from users';
 
-  connection.query(number_of_users_query, function (error, results, fields) {
-    if (error) throw error;
-    app_data.push(results[0]);
-    res.render('app', { app_data: app_data });
-  });
+    connection.query(number_of_users_query, function (error, results, fields) {
+      if (error) throw error;
+      app_data.push(results[0]);
+      res.render('app', { app_data: app_data });
+    });
 
-  connection.end(function (err) {
-    if (err) {
-      console.error('error ending connection: ' + err.stack);
-      return;
-    }
-
-    console.log('Connection ended!');
+    connection.release();
   });
 });
 
@@ -61,42 +48,55 @@ app.post('/register', function (req, res) {
     email: req.body.email,
   };
 
-  let user_email_q = 'SELECT COUNT(*) as total FROM `users` WHERE `email` = ?';
+  if (email === '' || interest_id === undefined) {
+    return res.redirect('/');
+  }
+  console.log(interest_id);
 
-  connection.query(user_email_q, [email], function (err, result) {
+  pool.getConnection(function (err, connection) {
     if (err) throw err;
 
-    if (result[0].total == 1) {
-      // alert('This email has already been used.');
-      res.redirect('/');
-    } else {
-      connection.query('INSERT INTO users SET ?', person, function (
-        err,
-        result
-      ) {
-        if (err) throw err;
-      });
+    let user_email_q =
+      'SELECT COUNT(*) as total FROM `users` WHERE `email` = ?';
 
-      let user_id_q = 'SELECT `id` FROM `users` WHERE `email` = ?';
+    connection.query(user_email_q, [email], function (error, result) {
+      if (error) throw error;
 
-      connection.query(user_id_q, [email], function (err, result) {
-        if (err) throw err;
+      if (result[0].total == 1) {
+        // alert('This email has already been used.');
+        return res.redirect('/');
+      } else {
+        connection.query('INSERT INTO users SET ?', person, function (
+          error,
+          result
+        ) {
+          if (error) throw error;
+        });
 
-        // insert into user_interests table
-        if (typeof interest_id != 'undefined') {
-          let data = [];
-          for (let i = 0; i < interest_id.length; i++) {
-            data.push([result[0].id, interest_id[i]]);
+        let user_id_q = 'SELECT `id` FROM `users` WHERE `email` = ?';
+
+        connection.query(user_id_q, [email], function (error, result) {
+          if (error) throw error;
+
+          // insert into user_interests table
+          if (typeof interest_id != 'undefined') {
+            let data = [];
+            for (let i = 0; i < interest_id.length; i++) {
+              data.push([result[0].id, interest_id[i]]);
+            }
+
+            let q =
+              'INSERT INTO user_interests (user_id, interest_id) VALUES ?';
+            connection.query(q, [data], function (error, result) {
+              if (error) throw error;
+            });
           }
+          return res.redirect('/');
+        });
+      }
+    });
 
-          let q = 'INSERT INTO user_interests (user_id, interest_id) VALUES ?';
-          connection.query(q, [data], function (err, result) {
-            if (err) throw err;
-          });
-        }
-        res.redirect('/');
-      });
-    }
+    connection.release();
   });
 });
 
